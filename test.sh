cf256db3cd248c320656111cc975aea823ccd6af
kerusu <kerusu1017@yahoo.co.jp>
2023-01-19 18:12:00 +0900

    1つの変数を実装

--- test.sh
+++ test.sh
@@ -16,36 +16,37 @@ assert() {
   fi
 }
 
-# assert "0;" 0
-# assert "42;" 42
-# assert "5+20-4;" 21
-# assert ' 12 + 34 - 5 ;' 41
-# assert '5+6*7;' 47
-# assert '5*(9-6);' 15
-# assert '(3+5)/2;' 4
-# assert '-10+20;' 10
-# assert '- -10;' 10
-# assert '- - +10;' 10
+assert "0;" 0
+assert "42;" 42
+assert "5+20-4;" 21
+assert ' 12 + 34 - 5 ;' 41
+assert '5+6*7;' 47
+assert '5*(9-6);' 15
+assert '(3+5)/2;' 4
+assert '-10+20;' 10
+assert '- -10;' 10
+assert '- - +10;' 10
 
-# assert '0==1;' 0
-# assert '42==42;' 1
-# assert '0!=1;' 1
-# assert '42!=42;' 0
+assert '0==1;' 0
+assert '42==42;' 1
+assert '0!=1;' 1
+assert '42!=42;' 0
 
-# assert '0<1;' 1
-# assert '1<1;' 0
-# assert '2<1;' 0
-# assert '0<=1;' 1
-# assert '1<=1;' 1
-# assert '2<=1;' 0
+assert '0<1;' 1
+assert '1<1;' 0
+assert '2<1;' 0
+assert '0<=1;' 1
+assert '1<=1;' 1
+assert '2<=1;' 0
 
-# assert '1>0;' 1
-# assert '1>1;' 0
-# assert '1>2;' 0
-# assert '1>=0;' 1
-# assert '1>=1;' 1
-# assert '1>=2;' 0
+assert '1>0;' 1
+assert '1>1;' 0
+assert '1>2;' 0
+assert '1>=0;' 1
+assert '1>=1;' 1
+assert '1>=2;' 0
 
-assert "1; 2; 3;" 3;
+
+assert "a=2;b=3;a+b;" 5;
 
 echo OK
--- tokenize.c
+++ tokenize.c
@@ -1,7 +1,8 @@
 #include "kaede.h"
 
 // Reports an error and exit.
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
@@ -12,7 +13,8 @@ void error(char *fmt, ...) {
 char *user_input;
 
 // Reports an error location and exit.
-void error_at(char *loc, char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...)
+{
     va_list ap;
     va_start(ap, fmt);
 
@@ -26,7 +28,8 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 // Create a new token and add it as the next token of `cur`.
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len)
+{
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
     tok->str = str;
@@ -38,41 +41,48 @@ Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
 bool startswith(char *p, char *q) { return memcmp(p, q, strlen(q)) == 0; }
 
 // Tokenize `user_input` and returns new tokens.
-Token *tokenize() {
+Token *tokenize()
+{
     char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
 
-    while (*p) {
+    while (*p)
+    {
         // Skip whitespace characters.
-        if (isspace(*p)) {
+        if (isspace(*p))
+        {
             p++;
             continue;
         }
 
         // Multi-letter punctuator
         if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") ||
-            startswith(p, ">=")) {
+            startswith(p, ">="))
+        {
             cur = new_token(TK_RESERVED, cur, p, 2);
             p += 2;
             continue;
         }
 
         // Single-letter punctuator
-        if (strchr("+-*/()<>;", *p)) {
+        if (strchr("+-*/()<>=;", *p))
+        {
             cur = new_token(TK_RESERVED, cur, p++, 1);
             continue;
         }
 
-        // if ('a' <= *p && *p <= 'z') {
-        //     cur = new_token(TK_IDENT, cur, p++, 1);
-        //     cur->len = 1;
-        //     continue;
-        // }
+        if ('a' <= *p && *p <= 'z')
+        {
+            cur = new_token(TK_IDENT, cur, p++, 1);
+            cur->len = 1;
+            continue;
+        }
 
         // Integer literal
-        if (isdigit(*p)) {
+        if (isdigit(*p))
+        {
             cur = new_token(TK_NUM, cur, p, 0);
             char *q = p;
             char *tmp;
